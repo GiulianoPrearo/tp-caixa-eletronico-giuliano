f9682950758afbecae6d8c2d54881e401db0b72d
prismato <giuprearo@outlook.com>
2021-05-12 20:09:23 -0300

    Commit Inicial

---
+++ CaixaEletronico/src/caixaeletronico/CaixaEletronico.java
@@ -0,0 +1,48 @@
+
+package caixaeletronico;
+
+public class CaixaEletronico {
+
+    public static void main(String[] args) {
+        
+       /* 
+        CRIAÇÃO DA INSTANCIA DA CLASSE CONTA 
+        TIPO
+        NOME DE VARIÁVEL DE INSTANCIA
+        DEFINIR A CLASSE QUE SERÁ INTANCIADA
+        */
+       //Conta objConta = new Conta();
+       
+       /* ACESSANDO OS ATRIBUTOS */
+//        System.out.println("AGENCIA: " + objConta.agencia);
+//        System.out.println("CONTA: " + objConta.nroConta);
+//        System.out.println("SALDO: " + objConta.saldo);
+    
+       /* ACESSANDO OS MÉTODOS */
+       //objConta.verificarSaldo();
+       
+       //objConta.realizarDeposito(500);
+       //objConta.verificarSaldo();
+//       
+       //objConta.realizarSaque(320.50f);
+       //objConta.verificarSaldo();
+       
+       /* CRIAÇÃO DO OBJETO DE MENU */
+//       menu2 objMenu = new menu2();
+      
+       /*
+        1 - SALDO
+        2 - DEPÓSITO
+        3 - SAQUE
+       */
+       
+       
+//       objMenu.executarMenu();
+
+TesteWhile objWhile = new TesteWhile();
+objWhile.testeLoop();
+       
+       
+    }
+    
+}
---
+++ CaixaEletronico/src/caixaeletronico/TesteWhile.java
@@ -0,0 +1,20 @@
+package caixaeletronico;
+
+public class TesteWhile {
+    /*
+    LOOP WHILE COM VARIÁVEL DE CONTADOR
+    */
+    
+    public void testeLoop(){
+        
+        int cont = 0;
+        
+        while (cont <=10){
+            System.out.println("O VALOR DA VARIÁVEL CONT: " + cont);
+            
+            cont++;
+            
+        }
+    }
+    
+}
